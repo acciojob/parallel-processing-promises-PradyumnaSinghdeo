401e3b3b503ef40c6ce6d41c3440d1fe61289a4a
Pradyumna Singh Deo <87163520+PradyumnaSinghdeo@users.noreply.github.com>
2024-10-25 20:24:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
@@ -7,3 +6,29 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener("click", () => {
+  downloadImages(images)
+    .then((imageElements) => {
+      imageElements.forEach((img) => {
+        output.appendChild(img);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      output.innerHTML = `<p style="color: red;">${error}</p>`;
+    });
+});
+
+function downloadImages(images) {
+  const imagePromises = images.map((image) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = image.url;
+      img.onload = () => resolve(img);
+      img.onerror = () => reject(`Failed to load image's URL: ${image.url}`);
+    });
+  });
+
+  return Promise.all(imagePromises);
+}
